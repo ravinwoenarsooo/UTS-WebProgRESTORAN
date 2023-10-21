1cb90d55697f5926bcd7fb754eca7b8856cf26dc
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-10-21 17:48:12 +0700

    f

--- RestoranUTS/resources/views/home.blade.php
+++ RestoranUTS/resources/views/home.blade.php
@@ -6,19 +6,16 @@
     @vite('resources/css/app.css')
     <title>Home</title>
     <style>
-        /* Add this CSS to style the fixed navigation bar */
         nav {
             position: fixed;
             top: 0;
             left: 0;
             right: 0;
-            background-color: #FF0000; /* Set your desired background color */
-            z-index: 1000; /* Adjust the z-index to ensure it's above other elements */
+            background-color: #FF0000; 
+            z-index: 1000; 
         }
-
-        /* Add some padding to the body to prevent content from being hidden under the fixed navigation bar */
         body {
-            padding-top: 64px; /* Adjust this value based on your navigation bar's height */
+            padding-top: 64px; 
         }
     </style>
 </head>
