b5ed37c3823e945c31fdc3833a8d99d145bd683c
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-10-21 20:32:12 +0700

    Update shoppingcart.blade.php

--- RestoranUTS/resources/views/shoppingcart.blade.php
+++ RestoranUTS/resources/views/shoppingcart.blade.php
@@ -107,12 +107,12 @@
                                 </div>
                                 <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                                 <div class="mt-6">
-                                    <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+                                    <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-red-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-black">Checkout</a>
                                 </div>
                                 <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                                     <p>
                                         or
-                                        <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
+                                        <button type="button" class="font-medium text-red-500 hover:text-red-500">
                                             Continue Shopping
                                             <span aria-hidden="true"> &rarr;</span>
                                         </button>
