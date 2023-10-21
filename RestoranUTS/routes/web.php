436cd805b5693a55b17354ede05ee809ce86f457
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-10-21 10:30:27 +0700

    update

--- RestoranUTS/routes/web.php
+++ RestoranUTS/routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/home', function(){
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
