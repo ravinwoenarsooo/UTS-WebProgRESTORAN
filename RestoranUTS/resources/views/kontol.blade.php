034e65a59ccb189885d557abde08fb5360bde6fc
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-10-21 12:00:42 +0700

    kontol

---
+++ RestoranUTS/resources/views/kontol.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold text-red-300 hover:text-blue-500">
+    Home
+    </h1>
+</body>
+</html>
--- RestoranUTS/routes/web.php
+++ RestoranUTS/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/kontol', function () {
+    return view('kontol');
+});
