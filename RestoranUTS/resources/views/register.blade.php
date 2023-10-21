571e785944087ded017dc2e7033a1df9f9dd38d0
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-10-21 16:39:01 +0700

    f

--- RestoranUTS/resources/views/register.blade.php
+++ RestoranUTS/resources/views/register.blade.php
@@ -18,62 +18,62 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6 p-6 bg-white rounded-md shadow-md" action="#" method="POST">
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
         <div class="mt-2">
-            <input id="first-name" name="first-name" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="first-name" name="first-name" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
         <div class="mt-2">
-            <input id="last-name" name="last-name" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="last-name" name="last-name" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="birth-date" class="block text-sm font-medium leading-6 text-gray-900">Birth Date</label>
         <div class="mt-2">
-            <input id="birth-date" name="birth-date" type="date" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="birth-date" name="birth-date" type="date" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for "gender" class="block text-sm font-medium leading-6 text-gray-900">Gender</label>
         <div class="mt-2">
-            <select id="gender" name="gender" class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <select id="gender" name="gender" class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
             </select>
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
         <div class="mt-2">
-            <input id="username" name="username" type="username" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="username" name="username" type="username" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
-            <input id="email" name "email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="email" name "email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4">
         <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
         </div>
         <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus-ring-inset focus-ring-red-500 sm:text-sm sm:leading-6">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
         </div>
         <div class="mt-2">
-            <input id="confirm-password" name="confirm-password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+            <input id="confirm-password" name="confirm-password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <label for="captcha" class="block text-sm font-medium leading-6 text-gray-900">Captcha</label>
         <div class="mt-2">
             <mark style="background: #F44336!important">
@@ -83,11 +83,11 @@
                 $_SESSION['captcha'] = $captchaText;
                 echo $captchaText;
                 ?>
-                <input id="captcha" name="captcha" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
+                <input id="captcha" name="captcha" type="text" required class="block w-full rounded-md border-0 py-2 pl-2 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
             </mark>
         </div>
       </div>
-      <div class="mb-4 ml-4"> <!-- Added margin to the left and bottom of the input -->
+      <div class="mb-4 ml-4"> 
         <button type="submit" class="flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Sign Up</button>
       </div>
     </form>
