ff79181fbaf75bab43899bbdd276edcf42996cd2
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-10-21 16:16:31 +0700

    f

--- RestoranUTS/resources/views/register.blade.php
+++ RestoranUTS/resources/views/register.blade.php
@@ -39,7 +39,6 @@
             <select id="gender" name="gender" class="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
-                <option value="other">Other</option>
             </select>
         </div>
       </div>
