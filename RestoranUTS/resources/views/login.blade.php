4efcbdd1da37aa4deccc2f919bb0ab1ba5525ecf
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-10-21 14:07:34 +0700

    uo

--- RestoranUTS/resources/views/login.blade.php
+++ RestoranUTS/resources/views/login.blade.php
@@ -9,7 +9,7 @@
 <body>
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <img class="mx-auto h-10 w-auto" src="https://1000marcas.net/wp-content/uploads/2022/05/Akatsuki-Logo.png" alt="Your Company">
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
   </div>
 
@@ -26,7 +26,7 @@
         <div class="flex items-center justify-between">
           <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            <a href="#" class="font-semibold text-blackl hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
         <div class="mt-2">
@@ -35,13 +35,13 @@
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
       </div>
     </form>
 
     <p class="mt-10 text-center text-sm text-gray-500">
       Are You New?
-      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign Up Here</a>
+      <a href="#" class="font-semibold leading-6 text-black hover:text-indigo-500">Sign Up Here</a>
     </p>
   </div>
 </div>
