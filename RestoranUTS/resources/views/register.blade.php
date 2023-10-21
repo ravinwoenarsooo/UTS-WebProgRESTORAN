d9251451230e68184c43307e5771409ecce85f3c
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-10-21 12:55:00 +0700

    gg

---
+++ RestoranUTS/resources/views/register.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Page</title>
+</head>
+<body>
+    <h2>Registration Form</h2>
+    <form method="post" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br><br>
+        
+        <label for="confirm_password">Confirm Password:</label>
+        <input type="password" name="confirm_password" required><br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+        
+        <input type="submit" name="register" value="Register">
+    </form>
+
+    <?php
+    if (isset($_POST['register'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        $email = $_POST['email'];
+
+        if ($password !== $confirm_password) {
+            echo "Password and confirm password do not match.";
+        } else {
+            $db = new mysqli("localhost", "username", "password", "database_name");
+            
+            if ($db->connect_error) {
+                die("Connection failed: " . $db->connect_error);
+            }
+
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+            $query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("sss", $username, $hashed_password, $email);
+
+            if ($stmt->execute()) {
+                echo "Registration successful!";
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+
+            $stmt->close();
+            $db->close();
+        }
+    }
+    ?>
+</body>
+</html>
