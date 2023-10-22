609f99e9e6224ae340a3bcde0c4693146f151026
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-10-22 15:06:36 +0700

    update pertama

--- RestoranUTS/routes/web.php
+++ RestoranUTS/routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\NewPageController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Auth\LoginController;
 use App\Models\User;
 
 /*
