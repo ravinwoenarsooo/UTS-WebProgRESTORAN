beaf76b09dd96898071da250c2a028a7d2b5fba4
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-10-22 11:12:36 +0700

    Update 2014_10_12_000000_create_users_table.php

--- RestoranUTS/database/migrations/2014_10_12_000000_create_users_table.php
+++ RestoranUTS/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,10 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id()->zerofill();
+            $table->string('first_name',20);
+            $table->string('last_name',20);
+            $table->date('birth_date');
+            $table->string('gender');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
